551b8e2943e10f2bd689008862bb01d23741226d
Tanveer Hossain Munim <munim987654321@gmail.com>
2022-11-30 18:30:33 +0600

    Fixed user priviledge

--- user_api/models.py
+++ user_api/models.py
@@ -5,10 +5,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 AbstractUser._meta.get_field('email')._unique = True
 
+USER_OPTIONS = (
+    ('user', 'user'),
+    ('admin', 'admin'),
+    ('super-admin', 'super-admin')
+)
+
 class SiteUser(AbstractUser):
     shipping_address = models.CharField(max_length=100, blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
-    special_user = models.BooleanField(default=False)
+    special_user = models.CharField(max_length=20, choices=USER_OPTIONS, default='user')
     
     class Meta:
         unique_together = ('username', 'email')
